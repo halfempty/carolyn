fab233b4e3e1099dbfeea0ef9cf6a0cbc1de031e
Marty Spellerberg <marty@halfempty.com>
2012-11-19 16:13:52 -0800

    Skins work

--- functions.php
+++ functions.php
@@ -1,6 +1,32 @@
 <?php
 add_action('init', 'register_custom_menu');
- 
+
+function carolyn_theme_scripts_method() {
+
+    $defaultstyle = get_bloginfo('stylesheet_url'); 
+    wp_register_style('defaultstyle',$defaultstyle);
+    wp_enqueue_style( 'defaultstyle');
+
+	$options = get_option('carolyn_theme_options');
+
+ 	if ( isset( $options['skincss'] ) && $options['skincss'] !== '' ) :
+	    $skinstyle = get_template_directory_uri() . '/skins/'. $options['skincss'] . '.css'; 
+	else :
+	    $skinstyle = get_template_directory_uri() . '/skins/carolyndrake.css'; 
+	endif;
+
+    wp_register_style('skinstyle',$skinstyle);
+    wp_enqueue_style( 'skinstyle');
+
+
+}
+
+add_action('wp_enqueue_scripts', 'carolyn_theme_scripts_method');
+
+
+
+
+
 function register_custom_menu() {
 	register_nav_menu('navigation', __('Navigation Menu'));
 }
@@ -16,12 +42,12 @@ if ( function_exists( 'add_theme_support' ) ) {
 // end
 
 
+
+
+
+
 // http://ottopress.com/2012/how-to-leverage-the-theme-customizer-in-your-own-themes/
 // http://ottopress.com/2012/making-a-custom-control-for-the-theme-customizer/
-
-
-
-
 
 
 add_action( 'customize_register', 'carolyn_customize_register' );
@@ -34,7 +60,7 @@ function carolyn_customize_register($wp_customize) {
 		public function render_content() {
 			?>
 			<label>
-			<span class="carolyn-control-title"><?php echo esc_html( $this->label ); ?></span>
+			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
 			<textarea rows="5" style="width:100%;" <?php $this->link(); ?>><?php echo esc_textarea( $this->value() ); ?></textarea>
 			</label>
 			<?php
@@ -42,8 +68,90 @@ function carolyn_customize_register($wp_customize) {
 	}
 
 
+	class Carolyn_Select_Control extends WP_Customize_Control {
+		public $type = 'select';
+
+		public function render_content() {
+			?>
+			<label>
+			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+			<select <?php $this->link(); ?>>
+				<?php $skins = array(); ?>
+				<?php $skins[] = array(
+					'slug'           => 'carolyndrake',
+					'nicename'     => 'Carolyn Drake',
+				); ?>
+				<?php $skins[] = array(
+					'slug'           => 'samueljames',
+					'nicename'     => 'Samuel James',
+				); ?>
+				<?php $skins[] = array(
+					'slug'           => 'honoriastarbuck',
+					'nicename'     => 'Honoria Starbuck',
+				); ?>
+
+				<?php foreach ($skins as $skin ) : ?>
+				<option value="<?php echo $skin['slug'] ?>"
+					<?php if ( $skin['slug'] == esc_textarea( $this->value() ) ) echo ' selected="selected"'; ?>
+				><?php echo $skin['nicename'] ?></option>
+				<?php endforeach; ?>
+			</select>
+			</label>
+			<?php
+		}
+	}
+
+	/* Layout and Design */
+
+	$wp_customize->add_section( 'carolyn_skin', array(
+		'title'          => __( 'Layout &amp; Design', 'carolyn' ),
+		'priority'       => 35,
+	) );
+
+	$wp_customize->add_setting( 'carolyn_theme_options[skincss]', array(
+		'type'           => 'option',
+		'capability'     => 'edit_theme_options',
+		'transport'      => 'postMessage',
+	) );
+
+	$wp_customize->add_control( new Carolyn_Select_Control( $wp_customize, 'skin', array(
+		'settings' => 'carolyn_theme_options[skincss]',
+		'label'    => __( 'Layout' ),
+		'section'  => 'carolyn_skin',
+		'type'     => 'text',
+	) ) );
+
+	$wp_customize->add_setting( 'carolyn_theme_options[typekit_js]', array(
+		'type'           => 'option',
+		'capability'     => 'edit_theme_options',
+		'transport'      => 'postMessage',
+	) );
+
+	$wp_customize->add_control( new Carolyn_Textarea_Control( $wp_customize, 'typekit_js', array(
+		'settings' => 'carolyn_theme_options[typekit_js]',
+		'label'    => __( 'Fonts JavaScript' ),
+		'section'  => 'carolyn_skin',
+	) ) );
+
+
+	$wp_customize->add_setting( 'carolyn_theme_options[typekit_css]', array(
+		'type'           => 'option',
+		'capability'     => 'edit_theme_options',
+		'transport'      => 'postMessage',
+	) );
+	
+	$wp_customize->add_control( new Carolyn_Textarea_Control( $wp_customize, 'typekit_css', array(
+		'settings' => 'carolyn_theme_options[typekit_css]',
+		'label'    => __( 'Fonts CSS' ),
+		'section'  => 'carolyn_skin',
+	) ) );
+
+
+
+	/* Carolyn Legacy */
+
 	$wp_customize->add_section( 'carolyn_menu_style', array(
-		'title'          => __( 'Menu Style', 'carolyn' ),
+		'title'          => __( 'Carolyn Legacy', 'carolyn' ),
 		'priority'       => 35,
 	) );
 
@@ -127,41 +235,6 @@ function carolyn_customize_register($wp_customize) {
 		'capability'     => 'edit_theme_options',
 		'transport'      => 'postMessage',
 	) );
-
-
-	$wp_customize->add_section( 'carolyn_font', array(
-		'title'          => __( 'Fonts', 'carolyn' ),
-		'priority'       => 35,
-	) );
-
-
-	$wp_customize->add_setting( 'carolyn_theme_options[typekit_js]', array(
-		'type'           => 'option',
-		'capability'     => 'edit_theme_options',
-		'transport'      => 'postMessage',
-	) );
-
-	$wp_customize->add_control( new Carolyn_Textarea_Control( $wp_customize, 'typekit_js', array(
-		'settings' => 'carolyn_theme_options[typekit_js]',
-		'label'    => __( 'Typekit JavaScript' ),
-		'section'  => 'carolyn_font',
-	) ) );
-
-
-	$wp_customize->add_setting( 'carolyn_theme_options[typekit_css]', array(
-		'type'           => 'option',
-		'capability'     => 'edit_theme_options',
-		'transport'      => 'postMessage',
-	) );
-	
-	$wp_customize->add_control( new Carolyn_Textarea_Control( $wp_customize, 'typekit_css', array(
-		'settings' => 'carolyn_theme_options[typekit_css]',
-		'label'    => __( 'Typekit CSS' ),
-		'section'  => 'carolyn_font',
-	) ) );
-
-
-
 
 }
 
--- header.php
+++ header.php
@@ -15,46 +15,18 @@
 			echo ' &mdash; ' . sprintf( __( 'Page %s' ), max( $paged, $page ) );
 		?></title>
 
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-	<?php $options = get_option('carolyn_theme_options'); ?>
+		<?php $options = get_option('carolyn_theme_options'); ?>
 
-	<?php if ( isset( $options['typekit_js'] ) && $options['typekit_js'] !== '' ) echo $options['typekit_js'] ?>
+		<?php if ( isset( $options['typekit_js'] ) && $options['typekit_js'] !== '' ) echo $options['typekit_js'] ?>
 
-
-	<style>
-
-	<?php if ( isset( $options['typekit_css'] ) && $options['typekit_css'] !== '' ) echo $options['typekit_css'] ?>
-	    
-	<?php if ( isset( $options['nav_width'] ) && $options['nav_width'] !== '' ) echo "#header { width: " . $options['nav_width'] . "px }" ?>
-		
-	<?php if ( $options['nav_align'] == 'right' ) : ?>
-		#header ul { margin: 0; text-align: right; }
-		#header ul li {padding: 0 0 0 1.6em; }
-		.subnav ul li a { padding: .5em; }
-
-	<?php else : ?>
-		#header ul { margin: 0 200px; text-align: center; }
-		#header ul li {padding: 0 .8em; }
-		.subnav ul li a { padding: .5em 0 .5em 1em; }
-	<?php endif; ?>
+		<?php if ( isset( $options['typekit_css'] ) && $options['typekit_css'] !== '' ) : ?>
+			<style>			
+				<?php echo $options['typekit_css'] ?>
+			</style>
+		<?php endif ?>
 	
-		.current-menu-item a, .current_page_ancestor a, .current-post-ancestor a { 
-			color: <?php echo $options['menu_highlight_color']; ?>; 
-			 <?php if ( $options['menu_highlight_uppercase'] == 1 )  echo 'text-transform: uppercase;' ?>
-			 <?php if ( $options['menu_highlight_weight'] == 1 )  echo 'font-weight: bold;' ?>
-		}
+		<?php wp_head(); ?>
 
-		#gallerywrapper, .gallery-fade {
-		overflow: hidden;
-		margin: auto;
-		position: relative;
-		}
-
-
-
-	</style>
-
-	<?php wp_head(); ?>
 </head>
 <body>
 <div class="page">	
